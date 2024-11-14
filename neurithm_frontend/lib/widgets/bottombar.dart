40d2816f707ec61dc18ddd70a32039e7fd44b0a2
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-11-14 18:13:32 +0200

    added navigations

--- neurithm_frontend/lib/widgets/bottombar.dart
+++ neurithm_frontend/lib/widgets/bottombar.dart
@@ -1,32 +1,68 @@
 import 'package:flutter/material.dart';
+import '../screens/history.dart'; 
+import '../screens/homepage.dart'; 
+import '../screens/devices.dart'; 
+import '../screens/settings.dart'; 
 
 
-BottomNavigationBar bottomappBar() {
-  return BottomNavigationBar(
-    backgroundColor: Color.fromARGB(
-        255, 70, 100, 166), // Matching color with dark blue gradient
-    selectedItemColor:
-        Color.fromARGB(255, 255, 255, 255), // Light blue for selected icon
-    unselectedItemColor:
-        Color.fromARGB(255, 168, 167, 167), // Gray for unselected icons
-    items: const [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: 'Home',
+class HomeScreen extends StatelessWidget {
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DevicesPage()),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SettingsPage()),
+        );
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HistoryPage()),
+        );
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 70, 100, 166),
+        selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+        unselectedItemColor: const Color.fromARGB(255, 168, 167, 167),
+        onTap: (index) => _onItemTapped(context, index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices),
+            label: 'Devices',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
       ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.devices),
-        label: 'Devices',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: 'Settings',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.history),
-        label: 'History',
-      ),
-    ],
-    type: BottomNavigationBarType.fixed,
-  );
+    );
+  }
 }
