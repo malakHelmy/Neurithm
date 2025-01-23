6d79e716eac2ab4cbeb2af66cecf953ee8aa5f56
janahani <jana2100237@miuegypt.edu.eg>
2025-01-23 19:55:41 +0200

    add firebase to main.dart

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+//packages
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+//screens
+import 'screens/loginPage.dart';
+
+
+Future<void> main() async {
+  // Ensure Flutter bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with provided options
+  await Firebase.initializeApp(
+  options: const FirebaseOptions(
+    apiKey: "AIzaSyB_ZfiJIMRQv1mHBLOyM9hxnXqRn9A7DC8", // From "api_key"
+    authDomain: "neurithm-8ac92.firebaseapp.com", // Usually "project_id.firebaseapp.com"
+    databaseURL: "https://neurithm-8ac92-default-rtdb.firebaseio.com", // Constructed URL
+    projectId: "neurithm-8ac92", // From "project_id"
+    storageBucket: "neurithm-8ac92.firebasestorage.app", // From "storage_bucket"
+    messagingSenderId: "49235168369", // From "project_number"
+    appId: "1:49235168369:android:b283ac169a67724e455fd0", // From "mobilesdk_app_id"
+  ),
+);
+
+  runApp(ThoughtToSpeechApp());
+}
+
+class ThoughtToSpeechApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF1A2A3A),
+        primaryColor: Color(0xFF394B58),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
