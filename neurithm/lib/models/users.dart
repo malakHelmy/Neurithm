7054d5d42afff59516f2d9871d4c788b03880107
Yassmin-Ezzat <125555633+Yassmin-Ezzat@users.noreply.github.com>
2025-02-06 22:17:21 +0200

    Updated users model

--- neurithm/lib/models/users.dart
+++ neurithm/lib/models/users.dart
@@ -1,37 +1,16 @@
-class Users {
+abstract class Users {
   final String uid;
-  final String firstName;
-  final String lastName;
   final String email;
   final String password;
 
   Users({
     required this.uid,
-    required this.firstName,
-    required this.lastName,
     required this.email,
     required this.password,
   });
 
-  // Convert a User to a Map (for Firestore)
-  Map<String, dynamic> toMap() {
-    return {
-      'uid': uid,
-      'firstName': firstName,
-      'lastName': lastName,
-      'email': email,
-      'password': password,
-    };
-  }
+  void login();
+  void logout();
 
-  // Convert a Map to a User
-  factory Users.fromMap(Map<String, dynamic> map) {
-    return Users(
-      uid: map['uid'],
-      firstName: map['firstName'],
-      lastName: map['lastName'],
-      email: map['email'],
-      password: map['password'],
-    );
-  }
+  Map<String, dynamic> toMap();
 }
