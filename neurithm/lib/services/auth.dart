99e718d439b0d71d523e7ed01f1f0a5be61bbff9
Yassmin-Ezzat <125555633+Yassmin-Ezzat@users.noreply.github.com>
2025-02-06 23:07:03 +0200

    Updated authentication

--- neurithm/lib/services/auth.dart
+++ neurithm/lib/services/auth.dart
@@ -1,58 +1,58 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:neurithm/models/users.dart';
 import 'package:neurithm/models/patient.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Patient? get currentUser {
+  Future<Patient?> getCurrentUser() async {
     try {
       User? user = _auth.currentUser;
       if (user != null) {
-        return Patient(
-          uid: user.uid,
-          firstName: user.displayName?.split(' ')[0] ?? '',
-          lastName: user.displayName!.split(' ').length > 1
-              ? user.displayName!.split(' ')[1]
-              : '',
-          email: user.email ?? '',
-          password: '',
-        );
+        // Fetch user details from Firestore
+        DocumentSnapshot userDoc =
+            await _firestore.collection('patients').doc(user.uid).get();
+
+        if (userDoc.exists) {
+          return Patient(
+            uid: user.uid,
+            firstName: userDoc['firstName'] ?? '',
+            lastName: userDoc['lastName'] ?? '',
+            email: user.email ?? '',
+            password: '',
+          );
+        }
       }
     } catch (e) {
-      print("Error accessing current user: $e");
+      print("Error fetching current user: $e");
     }
     return null;
   }
 
   // Sign-Up with Email and Password
-  Future<bool> signUpWithEmailPassword(String firstName, String lastName, String email, String password) async {
+  Future<bool> signUpWithEmailPassword(
+      String firstName, String lastName, String email, String password) async {
     bool result = false;
     try {
       // Create the user with email and password
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: email, password: password);
       User? user = userCredential.user;
 
       if (user != null) {
-        // Create the Users model object
-        Patient newUser = Patient(
-          uid: user.uid,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: "",
-        );
+        // Store user data in Firestore
+        await _firestore.collection('patients').doc(user.uid).set({
+          'uid': user.uid,
+          'firstName': firstName,
+          'lastName': lastName,
+          'email': email,
+          'password': ''
+        });
 
-        // Add the user data to Firestore
-        await _firestore.collection('patients').doc(user.uid).set(newUser.toMap());
-
-        // Ensure user is authenticated before proceeding
-        if (_auth.currentUser != null) {
-          result = true;
-        }
+        result = true;
       }
     } catch (e) {
       print("Error during sign-up: $e");
@@ -64,8 +64,8 @@ class AuthMethods {
   Future<bool> signInWithEmailPassword(String email, String password) async {
     bool result = false;
     try {
-      // Sign in with email and password
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(email: email, password: password);
       User? user = userCredential.user;
 
       if (user != null) {
@@ -82,60 +82,54 @@ class AuthMethods {
     bool result = false;
     try {
       final GoogleSignIn googleSignIn = GoogleSignIn();
-      // Ensuring we sign out from any active session first
+
       if (await googleSignIn.isSignedIn()) {
         await googleSignIn.signOut();
       }
 
       final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-      if (googleUser == null) {
-        // User canceled the sign-in
-        return false;
-      }
+      if (googleUser == null) return false;
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      UserCredential userCredential = await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
       User? user = userCredential.user;
 
       if (user != null) {
         if (userCredential.additionalUserInfo!.isNewUser) {
-          // Add the data to Firestore
           await _firestore.collection('patients').doc(user.uid).set({
+            'uid': user.uid,
             'firstName': user.displayName?.split(' ')[0] ?? '',
             'lastName': user.displayName!.split(' ').length > 1
                 ? user.displayName!.split(' ')[1]
                 : '',
-            'uid': user.uid,
             'email': user.email,
             'password': ''
           });
         }
 
-        // Ensuring user is authenticated before proceeding
-        if (_auth.currentUser != null) {
-          result = true;
-        }
+        result = true;
       }
-      return result;
     } catch (e) {
       print("Error during Google Sign-In: $e");
-      return result;
     }
+    return result;
   }
 
   // Sign-Out Method
   Future<void> signOut() async {
     try {
-      await _auth.signOut(); // Sign out from Firebase Auth
+      await _auth.signOut();
 
       final GoogleSignIn googleSignIn = GoogleSignIn();
       if (await googleSignIn.isSignedIn()) {
-        await googleSignIn.signOut(); // Sign out from Google
+        await googleSignIn.signOut();
       }
     } catch (e) {
       print('Error during sign-out: $e');
