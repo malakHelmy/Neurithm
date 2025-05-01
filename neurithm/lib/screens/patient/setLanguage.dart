342292f14b3d87177eec15390e13afc0d9be00b5
fatimahhatem <106448000+fatimahelsayed@users.noreply.github.com>
2025-05-02 01:53:57 +0300

    adjustment in design

--- neurithm/lib/screens/patient/setLanguage.dart
+++ neurithm/lib/screens/patient/setLanguage.dart
@@ -4,15 +4,15 @@ import 'package:neurithm/widgets/appbar.dart';
 import 'package:neurithm/widgets/wavesBackground.dart';
 import 'package:provider/provider.dart';
 import 'package:neurithm/l10n/generated/app_localizations.dart';
-import 'package:neurithm/models/locale.dart'; 
+import 'package:neurithm/models/locale.dart';
 
 class SetLanguageScreen extends StatelessWidget {
   const SetLanguageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var t = AppLocalizations.of(context)!;  // Get localized strings
-    var selectedLocale = Localizations.localeOf(context).toString();  // Get current locale
+    var t = AppLocalizations.of(context)!;
+    var selectedLocale = Localizations.localeOf(context).toString();
 
     return Scaffold(
       body: Container(
@@ -20,9 +20,7 @@ class SetLanguageScreen extends StatelessWidget {
           children: [
             wavesBackground(getScreenWidth(context), getScreenHeight(context)),
             Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: spacing(15, getScreenHeight(context)),
-              ),
+              padding: EdgeInsets.all(spacing(16, getScreenHeight(context))),
               child: AppBar(
                 backgroundColor: Colors.transparent,
                 leading: IconButton(
@@ -39,78 +37,89 @@ class SetLanguageScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    t.chooseLanguage,
-                    style: const TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'Lato',
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  Consumer<LocaleModel>(
-                    builder: (context, localeModel, child) => ElevatedButton(
-                      onPressed: () {
-                        // Update the language when "English" is selected
-                        localeModel.set(Locale('en'));
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                      ),
-                      child: Text(
-                        t.english,  // Localized string for English
-                        style: const TextStyle(
-                          color: Color(0xFF1A2A3A),
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Consumer<LocaleModel>(
-                    builder: (context, localeModel, child) => ElevatedButton(
-                      onPressed: () {
-                        // Update the language when "Arabic" is selected
-                        localeModel.set(Locale('ar'));
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                      ),
-                      child: Text(
-                        t.arabic,  // Localized string for Arabic
-                        style: const TextStyle(
-                          color: Color(0xFF1A2A3A),
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  ),
-                  // Confirmation message after language selection
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Text(
-                      t.languageUpdated,  // Localized confirmation message
+            Padding(
+              padding: EdgeInsets.all(spacing(20, getScreenHeight(context))),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      t.chooseLanguage,
                       style: const TextStyle(
-                        fontSize: 24,
+                        fontSize: 32,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
+                        fontFamily: 'Lato',
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Consumer<LocaleModel>(
+                        builder: (context, localeModel, child) => ElevatedButton(
+                          onPressed: () {
+                            // Update the language when "English" is selected
+                            localeModel.set(Locale('en'));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 240, 240, 240),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                          ),
+                          child: Text(
+                            t.english, // Localized string for English
+                            style: const TextStyle(
+                              color: Color(0xFF1A2A3A),
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Consumer<LocaleModel>(
+                        builder: (context, localeModel, child) => ElevatedButton(
+                          onPressed: () {
+                            // Update the language when "Arabic" is selected
+                            localeModel.set(Locale('ar'));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 240, 240, 240),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                          ),
+                          child: Text(
+                            t.arabic, // Localized string for Arabic
+                            style: const TextStyle(
+                              color: Color(0xFF1A2A3A),
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Confirmation message after language selection
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Text(
+                        t.languageUpdated, // Localized confirmation message
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
